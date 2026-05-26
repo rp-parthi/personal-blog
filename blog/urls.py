a755b091cfd6ea31dd11c407b3ce6b94bac03a06
Parthiban <rp.paarthi@gmail.com>
2026-05-26 17:20:41 +0530

    add logout and remove delete from detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('tag/<str:tag_slug>/', views.TagPostListView.as_view(), name='tag-posts'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('resume/', views.resume, name='resume'),
 ]
